523ea25617df69b6461452f0a1f6a6e425f49d1a
Saulios <39069766+Saulios@users.noreply.github.com>
2021-12-18 18:33:12 +0100

    Initial update for FreeGameFindings bot

--- SteamGame.py
+++ SteamGame.py
@@ -11,53 +11,98 @@ class SteamGame:
         self.appID = appid
         self.url = 'https://store.steampowered.com/app/' + appid
         self.gamePage = BeautifulSoup(
-            requests.get(self.url, cookies={'birthtime': '640584001', 'lastagecheckage': '20-April-1990',
-                                            'mature_content': '1'}).text,
-            "html.parser")
-        self.json = json.loads(requests.get("https://store.steampowered.com/api/appdetails/?appids=" + appid + "&cc=us")
-                               .text)[appid]["data"]
+            requests.get(
+                self.url,
+                cookies={
+                    "birthtime": "640584001",
+                    "lastagecheckage": "20-April-1990",
+                    "mature_content": "1",
+                }).text,
+            "html.parser",
+        )
+        self.json = json.loads(requests.get(
+            "https://store.steampowered.com/api/appdetails/?appids=" + appid + "&cc=us")
+                               .text)
+        if self.json[appid]["success"] is not True:
+            # appid invalid
+            return None
+        self.json = self.json[appid]["data"]
 
-        self.title = self.gamePage.title.string.replace(" on Steam", "")
+        self.title = self.title()
+        self.gettype = self.gettype()
         self.discountamount = self.discountamount()
         self.price = self.getprice()
+        self.asf = self.getasf()
         self.achievements = self.getachev()
         self.cards = self.getcards()
         self.unreleased = self.isunreleased()
         self.blurb = self.getDescriptionSnippet()
+        self.reviewsummary = self.reviewsummary()
+        self.reviewdetails = self.reviewdetails()
+        self.usertags = self.usertags()
+        self.genres = self.genres()
+        self.basegame = self.basegame()
+        self.releasedate = self.releasedate()
+        self.nsfw = self.nsfw()
+
+    def title(self):
+        return self.json["name"]
+
+    def gettype(self):
+        return self.json["type"]
 
     def discountamount(self):
-        amount = self.gamePage.find("div", class_="discount_pct")
-
-        if amount is not None:
-            return amount.string.strip()
+        if len(self.json["package_groups"]) != 0:
+            amount = self.json["package_groups"][0]["subs"][0]["percent_savings_text"]
+            amount = amount.strip()
+            if amount != "":
+                return amount
+            else:
+                return False
         else:
             return False
 
     def getprice(self):
-        if len(self.json["package_groups"]) is 0:
+        if len(self.json["package_groups"]) == 0:
             return "Free"
-        
-        return "$" + str(self.json["package_groups"]
-                         [0]
-                         ["subs"]
-                         [0]
-                         ["price_in_cents_with_discount"] / 100)
+
+        price = self.json["package_groups"][0]["subs"][0]["price_in_cents_with_discount"] / 100
+        if price == 0:
+            return "Free"
+        elif self.isfree():
+            return "Free"
+        else:
+            return "$" + str(price)
 
     def isfree(self):
         return self.json["is_free"]
+
+    def getasf(self):
+        app_id = self.appID
+        if (
+            len(self.json["package_groups"]) != 0
+            and self.json["package_groups"][0]["subs"][0]["is_free_license"]
+        ):
+            app_id = self.json["package_groups"][0]["subs"][0]["packageid"]
+        return str(app_id)
 
     def getachev(self):
         achblock = self.gamePage.find("div", id="achievement_block")
 
         if achblock is not None:
-            return re.sub(r"\D", "", achblock.contents[1].string).strip()  # Remove all non numbers
+            ach_number = re.sub(r"\D", "", achblock.contents[1].string).strip()  # Remove all non numbers
+            if ach_number == "":
+                return 0
+            else:
+                return ach_number
         else:
             return 0
 
     def getcards(self):
         category_block = self.gamePage.find("div", id="category_block")
 
-        if category_block is None: return 0
+        if category_block is None:
+            return 0
         if "Steam Trading Cards" in category_block.text:
             marketurl = 'https://steamcommunity.com/market/search?q=&category_753_Game%5B0%5D=tag_app_' + self.appID + '&category_753_cardborder%5B0%5D=tag_cardborder_0&category_753_item_class%5B0%5D=tag_item_class_2'
             marketpage = BeautifulSoup(requests.get(marketurl).text, "html.parser")
@@ -77,13 +122,128 @@ class SteamGame:
         unreleasedMajor = self.gamePage.find("div", class_="game_area_comingsoon")
 
         if unreleasedMajor is not None:
-            return unreleasedMajor.find("h1").string.strip()
+            return unreleasedMajor.find("h1").text.strip()
         else:
             return None
 
     def getDescriptionSnippet(self):
-        snippet = self.gamePage.find("div", class_="game_description_snippet")
+        snippet = self.json["short_description"]
+
         if snippet is None:
             return ""
 
-        return snippet.string.strip()
+        return snippet.strip()
+
+    def islearning(self):
+        return self.gamePage.find("div", class_="learning_about") is not None
+
+    def reviewsummary(self):
+        review_div = self.gamePage.find("div", {"id": "userReviews"})
+        review_div_agg = review_div.find("div", {"itemprop": "aggregateRating"})
+        summary = review_div_agg.find("span", {"class": "game_review_summary"})
+        if summary is not None:
+            return summary.string
+        else:
+            return "No user reviews"
+
+    def reviewdetails(self):
+        review_div = self.gamePage.find("div", {"id": "userReviews"})
+        review_div_agg = review_div.find("div", {"itemprop": "aggregateRating"})
+        details_span = review_div_agg.select('span[class*="responsive_reviewdesc"]')
+        details = next(iter(details_span), None)
+        if details is not None:
+            details_strip = details.contents[0].strip()
+            if details_strip != "- Need more user reviews to generate a score":
+                details = details_strip.replace("for this game ", "")
+                details = details.replace("- ", " (")
+                details = details.replace("positive.", "positive)")
+                return details
+            else:
+                return ""
+        else:
+            return ""
+
+    def usertags(self):
+        usertags = self.gamePage.find("div", class_="popular_tags")
+        usertags_a = usertags.find_all("a", {"class": "app_tag"})
+        if len(usertags_a) != 0:
+            length = 5
+            if len(usertags_a) < 5:
+                length = len(usertags_a)
+            result_tags = ""
+            for tag in usertags_a[0:length]:
+                usertag_strip = tag.text.strip()
+                result_tags += usertag_strip + ", "
+            return result_tags.strip().removesuffix(",")
+        else:
+            return False
+
+    def genres(self):
+        if "genres" in self.json:
+            genres = self.json["genres"]
+            length = 4
+            genres_result = ""
+            if len(genres) < 4:
+                length = len(genres)
+            for genre in genres[0:length]:
+                genre_strip = genre["description"].strip()
+                genres_result += genre_strip + ", "
+            return genres_result.strip().removesuffix(",")
+        else:
+            return False
+
+    def basegame(self):
+        if "fullgame" in self.json:
+            basegame = self.json["fullgame"]
+            appid = basegame["appid"]
+            basegame_data = json.loads(requests.get(
+                "https://store.steampowered.com/api/appdetails/?appids=" + appid + "&cc=us")
+                            .text)[appid]["data"]
+
+            def basegameisfree():
+                return basegame_data["is_free"]
+
+            def basegameprice(appid):
+                if len(basegame_data["package_groups"]) == 0:
+                    return "Free"
+
+                price = basegame_data["package_groups"][0]["subs"][0]["price_in_cents_with_discount"] / 100
+                if price == 0:
+                    return "Free"
+                elif basegameisfree():
+                    return "Free"
+                else:
+                    return "$" + str(price)
+
+            def discountamount():
+                if len(basegame_data["package_groups"]) != 0:
+                    amount = basegame_data["package_groups"][0]["subs"][0]["percent_savings_text"]
+                    amount = amount.strip()
+                    if amount != "":
+                        return amount
+                    else:
+                        return False
+                else:
+                    return False
+
+            price = basegameprice(appid)
+            free = basegameisfree()
+            discount = discountamount()
+            return appid, basegame["name"], price, free, discount
+
+    def releasedate(self):
+        if "release_date" in self.json:
+            date = self.json["release_date"]
+            if date["coming_soon"] is False and date["date"] != "":
+                return date["date"]
+            else:
+                return False
+        else:
+            return False
+
+    def nsfw(self):
+        nsfw = self.gamePage.find("div", class_="mature_content_notice")
+        if nsfw is not None:
+            return True
+        else:
+            return False
