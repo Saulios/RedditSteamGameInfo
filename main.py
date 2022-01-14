df9e36f655761118b707a53f7a47bddd4d8a9e14
Saulios <39069766+Saulios@users.noreply.github.com>
2022-01-14 22:00:57 +0100

    Combine genre/tags, remove duplicate code

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def buildcommenttext(g, removed):
     if isinstance(g.title, str):
         commenttext = ''
         if removed:
-            commenttext += '*Appears to be removed/banned from Steam. This is information from ' + g.date + ':*\n\n'
+            commenttext += '*Removed/banned from Steam - this is information from ' + g.date + ':*\n\n'
         commenttext += '**' + g.title + '**'
         if g.nsfw:
             commenttext += ' *(NSFW)*'
@@ -85,7 +85,7 @@ def buildcommenttext(g, removed):
             commenttext += '[Community Hub](https://steamcommunity.com/app/' + g.appID + ') | '
         commenttext += '[SteamDB](https://steamdb.info/app/' + g.appID + ')\n'
         if not g.gettype == "game" and g.basegame is not None:
-            commenttext += '* **' + g.basegame[1] + '** links (base game): '
+            commenttext += '* Game links (**' + g.basegame[1] + '**): '
             if removed:
                 commenttext += '[Store Page (archived)](' + g.basegame[6]
             else:
@@ -109,60 +109,43 @@ def buildcommenttext(g, removed):
                 commenttext += " * Isn't released yet\n"
             else:
                 commenttext += ' * ' + g.unreleasedtext + '\n'
-            if g.isearlyaccess:
-                commenttext += ' * Is an Early Access Game\n'
-            if g.genres:
-                commenttext += ' * Genre'
-                if g.usertags == "":
-                    commenttext += '/Tags'
-                commenttext += ': ' + g.genres + '\n'
-            if g.usertags and g.usertags != "":
-                commenttext += ' * Tags: ' + g.usertags + '\n'
-            if g.plusone:
-                commenttext += ' * Full game license (no beta testing) will'
-            else:
-                commenttext += ' * Will not'
-            commenttext += ' give +1 game count [^(what is +1?)](https://www.reddit.com/r/FreeGameFindings/wiki/faq#wiki_what_is_.2B1.3F)\n'
-        else:
-            if not removed:
-                commenttext += ' * '
-                if g.price[0] == "Free" and g.basegame is not None and g.basegame[2] == "Free":
-                    commenttext += 'Game & '
-                if g.gettype == "dlc":
-                    commenttext += 'DLC '
-                elif g.gettype == "music":
-                    commenttext += 'Soundtrack '
-                commenttext += 'Price: '
-                if g.price[1] != "":
-                    commenttext += '~~' + g.price[1] + '~~ '
-                commenttext += g.price[0]
-                if not g.isfree() and g.price[0] != ("Free" and "No price found"):
+        if not removed and not (g.unreleased and g.price[0] == "No price found"):
+            commenttext += ' * '
+            if g.price[0] == "Free" and g.basegame is not None and g.basegame[2] == "Free":
+                commenttext += 'Game & '
+            if g.gettype == "dlc":
+                commenttext += 'DLC '
+            elif g.gettype == "music":
+                commenttext += 'Soundtrack '
+            commenttext += 'Price: '
+            if g.price[1] != "":
+                commenttext += '~~' + g.price[1] + '~~ '
+            commenttext += g.price[0]
+            if not g.isfree() and g.price[0] != ("Free" and "No price found"):
+                commenttext += ' USD'
+                if g.discountamount:
+                    commenttext += ' (' + g.discountamount + ')'
+            commenttext += '\n'
+            if not g.gettype == "game" and g.basegame is not None and len(g.basegame) > 2 and (g.price[0] != "Free" or g.basegame[2] != "Free"):
+                commenttext += ' * Game Price: '
+                if g.basegame[3] != "":
+                    commenttext += '~~' + g.basegame[3] + '~~ '
+                commenttext += g.basegame[2]
+                if not g.basegame[4] and g.basegame[2] != ("Free" and "No price found"):
                     commenttext += ' USD'
-                    if g.discountamount:
-                        commenttext += ' (' + g.discountamount + ')'
+                    if g.basegame[5]:
+                        commenttext += ' (' + g.basegame[5] + ')'
                 commenttext += '\n'
-                if not g.gettype == "game" and g.basegame is not None and len(g.basegame) > 2 and g.price[0] != "Free" and g.basegame[2] != "Free":
-                    commenttext += ' * Game Price: '
-                    if g.basegame[3] != "":
-                        commenttext += '~~' + g.basegame[3] + '~~ '
-                    commenttext += g.basegame[2]
-                    if not g.basegame[4] and g.basegame[2] != ("Free" and "No price found"):
-                        commenttext += ' USD'
-                        if g.basegame[5]:
-                            commenttext += ' (' + g.basegame[5] + ')'
-                    commenttext += '\n'
-            if g.releasedate:
-                commenttext += ' * Release Date: ' + g.releasedate + '\n'
-            if g.isearlyaccess:
-                commenttext += ' * Is an Early Access Game\n'
-            if g.genres:
-                commenttext += ' * Genre'
-                if g.usertags == "":
-                    commenttext += '/Tags'
-                commenttext += ': ' + g.genres + '\n'
-            if g.usertags and g.usertags != "":
-                commenttext += ' * Tags: ' + g.usertags + '\n'
-            if g.gettype == "game":
+        if not g.unreleased and g.releasedate:
+            commenttext += ' * Release Date: ' + g.releasedate + '\n'
+        if g.isearlyaccess and g.gettype == "game":
+            commenttext += ' * Is an Early Access Game\n'
+        if g.usertags and g.usertags != "":
+            commenttext += ' * Genre/Tags: ' + g.usertags + '\n'
+        elif g.genres:
+            commenttext += ' * Genre: ' + g.genres + '\n'
+        if g.gettype == "game":
+            if not g.unreleased:
                 if int(g.achievements) != 0:
                     commenttext += ' * Has ' + str(g.achievements) + ' achievements\n'
                 if not type(g.cards) == int:
@@ -179,18 +162,20 @@ def buildcommenttext(g, removed):
                     if int(g.achievements) == 0:
                         commenttext += ' or achievements'
                     commenttext += '\n'
-                if g.plusone:
-                    commenttext += ' * Gives'
-                else:
-                    commenttext += ' * Does not give'
-                commenttext += ' +1 game count [^(what is +1?)](https://www.reddit.com/r/FreeGameFindings/wiki/faq#wiki_what_is_.2B1.3F)\n'
-            if g.isfree() or g.price[0] == "Free":
-                commenttext += ' * Can be added to ASF clients with `!addlicense asf '
-                if not g.gettype == "game" and g.basegame is not None and len(g.basegame) > 2 and g.basegame[4]:
-                    commenttext += "a/" + g.basegame[0] + ","
-                commenttext += g.asf[0] + '`\n'
-                if g.asf[1] == "sub":
-                    commenttext += ' * Can be added in browsers/mobile with `javascript:AddFreeLicense(' + g.asf[0].strip("s/") + ')`\n'
+            if not g.unreleased and g.plusone:
+                commenttext += ' * Gives'
+            elif g.unreleased and g.plusone:
+                commenttext += ' * Full game license (no beta testing) will give'
+            else:
+                commenttext += ' * Does not give'
+            commenttext += ' +1 game count [^(what is +1?)](https://www.reddit.com/r/FreeGameFindings/wiki/faq#wiki_what_is_.2B1.3F)\n'
+        if (g.isfree() or g.price[0] == "Free") and not g.unreleased:
+            commenttext += ' * Can be added to ASF clients with `!addlicense asf '
+            if not g.gettype == "game" and g.basegame is not None and len(g.basegame) > 2 and g.basegame[4]:
+                commenttext += "a/" + g.basegame[0] + ","
+            commenttext += g.asf[0] + '`\n'
+            if g.asf[1] == "sub":
+                commenttext += ' * Can be added in browsers/mobile with `javascript:AddFreeLicense(' + g.asf[0].strip("s/") + ')`\n'
         commenttext += '\n***\n'
         return commenttext
 
